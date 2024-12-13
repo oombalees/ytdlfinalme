a284af4513f0dd6fff6b99571718f35d065e0b48
Broad Way <83855234+oombalees@users.noreply.github.com>
2024-12-14 02:53:25 +0530

    Update telegram_vimeo_bot.py

--- telegram_vimeo_bot.py
+++ telegram_vimeo_bot.py
@@ -1,5 +1,4 @@
-import os
-import subprocess
+import logging
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
 
